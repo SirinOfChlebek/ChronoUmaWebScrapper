6a3f716cd90fe581f0d09df062037659b523b7eb
SirinOfChlebek <julia6szabla@gmail.com>
2026-03-20 17:50:46 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import gspread
+from google.oauth2.service_account import Credentials
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import undetected_chromedriver as uc
+import time
+
+
+driver = uc.Chrome()
+
+url = "https://chronogenesis.net/club_profile?circle_id=421521758355"
+driver.get(url)
+time.sleep(10)
+
+scopes = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+credentials = Credentials.from_service_account_file("credentials.json", scopes = scopes)
+admin = gspread.authorize(credentials)
+
+sheet = admin.open("UmaScrapper").sheet1
+
+rows = driver.find_elements(By.TAG_NAME, "tr")
+
+data = []
+for row in rows:
+    cells = row.find_elements(By.XPATH, ".//th | .//td")
+    row_data = []
+    for cell in cells:
+        text = cell.text.strip()
+        row_data.append(text)
+    if any(row_data):
+        data.append(row_data)
+
+
+sheet.clear()
+sheet.update(values = data, range_name = "A1")
+driver.quit()
+
